cabd7b61311d404b7bb6cf3dd07695e5d6f6307d
steadfast2201 <patilaniket953765@gmail.com>
2025-05-20 16:05:17 +0530

    Modified main.py file

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data 
+
 @app.get("/")
 def hello():
-    return {"Message":"Hello World !!"}
+    return {"Message":"Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"Message":"Campusx is an Education Platform where you can learn AI"}
+    return {"Message":"A fully functional API to manage patient records"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
