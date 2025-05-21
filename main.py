ecf00536aadd6d0c287eafadeb5ca7358017571f
steadfast2201 <patilaniket953765@gmail.com>
2025-05-21 13:08:34 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -24,10 +24,30 @@ def view():
     return data
 
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id: str):
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example = 'P001')):
     # load all the patients
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
-    return {'error':'patient not found'}
+    # return {'error':'patient not found'}
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by:str = Query(..., description='Sort on the basis of height, weight or BMI'), order: str = Query('asc', description='sort in asc and desc order')):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid field. Select from valid fields {valid_fields}")
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
