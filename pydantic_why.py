84f654e31fb95b0f10b431dd88efd59e57dcd57f
steadfast2201 <patilaniket953765@gmail.com>
2025-05-26 10:52:26 +0530

    Save local changes before pull

--- pydantic_why.py
+++ pydantic_why.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
 
-    name:str = Field(max_length=50)
+    name: Annotated[str, Field(max_length=50, title='Name of the Patient', description='Give the name of the patient in less than 50 chars', examples=['Nitish','Aniket'])]
     email: EmailStr
     linkdin_url: AnyUrl
     age:int = Field(gt=0 , lt=100)
     weight: float = Field(gt = 0)
-    married: bool
-    allergies: Optional[List[str]] = None
+    married: Annotated[bool, Field(default=None, description='Is the patient Married or not')]
+    allergies: Optional[List[str]] = Field(max_length=5)
     contact_details: Dict[str, str]
 
 def insert_patient_data(patient: Patient):
